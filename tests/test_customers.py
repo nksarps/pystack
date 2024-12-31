e26ba1535c3b152e63efdaf69e3b78600fa35bd8
kweku-xvi <nksarps@gmail.com>
2024-12-31 11:22:23 +0000

    added tests for the methods in customer api

---
+++ tests/test_customers.py
@@ -0,0 +1,135 @@
+import unittest
+from decouple import config
+from pystack import Customer
+
+
+class TestCustomer(unittest.TestCase):
+    def setUp(self):
+        self.test_key = config('TEST_KEY')
+        self.customer = Customer(self.test_key)
+        self.base_url = 'https://api.paystack.co'
+        self.expected_headers = {
+            'Authorization': f'Bearer {self.test_key}',
+            'Content-Type': 'application/json'
+        }
+
+    def test_create_customer(self):
+        email = 'mail@example.com'
+        first_name = 'Jane'
+        last_name = 'Doe'
+        phone = '08012345678'
+
+
+        response = self.customer.create_customer(email, first_name, last_name, phone)
+
+        data = {
+            'email': 'mail@example.com',    
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+        }
+
+        self.assertEqual(response['status'], True)
+        self.assertEqual(response['message'], 'Customer created')
+        self.assertEqual(response['data']['email'], data['email'])
+        self.assertEqual(response['data']['first_name'], data['first_name'])
+        self.assertEqual(response['data']['last_name'], data['last_name'])
+    
+    def test_list_customer(self):
+        response = self.customer.list_customer()
+
+        self.assertEqual(response['status'], True)
+        self.assertEqual(response['message'], 'Customers retrieved')
+        self.assertIsInstance(response['data'], list)
+
+    def test_fetch_customer(self):
+        email_or_code = 'mail@example.com'
+
+        response = self.customer.fetch_customer(email_or_code)
+        
+        self.assertEqual(response['status'], True)
+        self.assertEqual(response['message'], 'Customer retrieved')
+        self.assertEqual(response['data']['email'], email_or_code)
+        self.assertIsInstance(response['data'], dict)
+
+    def test_update_customer(self):
+        code = 'CUS_v139w7vtm31u4yy'
+        first_name = 'Jane'
+        last_name = 'Doe'
+
+        response = self.customer.update_customer(code, first_name, last_name)
+
+        data = {
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+        }
+
+        self.assertEqual(response['status'], True)
+        self.assertEqual(response['message'], 'Customer updated')
+        self.assertEqual(response['data']['first_name'], data['first_name'])
+        self.assertEqual(response['data']['last_name'], data['last_name'])
+    
+    # URL Not Found
+    def test_validate_customer(self):
+        code = 'CUS_v139w7vtm31u4yy'
+        first_name = 'Jane'
+        last_name = 'Doe'
+        identification_type = 'bank_account'
+        value = '12345678901'
+        country = 'NG'
+        bvn = '12345678901'
+        bank_code = '044'
+        account_number = '1234567890'
+
+        response = self.customer.validate_customer(code, first_name, last_name, identification_type, value, country, bvn, bank_code, account_number)
+
+        data = {
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+            'identification_type': 'BVN',
+            'value': '12345678901',
+            'country': 'NG',
+            'bvn': '12345678901',
+            'bank_code': '044',
+            'account_number': '1234567890'
+        }
+
+        self.assertEqual(response['status'], True)
+        self.assertEqual(response['message'], 'Customer Identification in progress')
+
+    def test_whitelist_customer(self):
+        code = 'CUS_v139w7vtm31u4yy'
+        response = self.customer.blacklist_customer(code)
+
+        data = {
+            'code': 'CUS_v139w7vtm31u4yy',
+        }
+
+        self.assertEqual(response['status'], True)
+        self.assertEqual(response['message'], 'Customer updated')
+
+    def test_blacklist_customer(self):
+        code = 'CUS_v139w7vtm31u4yy'
+        response = self.customer.blacklist_customer(code)
+
+        data = {
+            'code': 'CUS_v139w7vtm31u4yy',
+        }
+
+        self.assertEqual(response['status'], True)
+        self.assertEqual(response['message'], 'Customer updated')
+
+    # URL Not Found
+    def test_deactivate_customer(self):
+        authorization_code = 'AUTH_72btv547'
+
+        response = self.customer.deactivate_customer(authorization_code)
+
+        data = {
+            'authorization_code': 'AUTH_72btv547',
+        }
+
+        self.assertEqual(response['status'], True)
+        self.assertEqual(response['message'], 'Authorization has been deactivated')
+
+if __name__ == '__main__':
+    unittest.main()
