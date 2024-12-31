ce73b6d646accfb98febacb15145eba8394b56ac
kweku-xvi <nksarps@gmail.com>
2024-12-31 11:22:40 +0000

    __init__.py

--- pystack/__init__.py
+++ pystack/__init__.py
@@ -1,5 +1,6 @@
 from .base import Base
 from .errors import PyStackError, MissingSecretKeyError, InvalidMethodError, InvalidDataError
 from .transactions import Transaction
+from .customers import Customer
 
 __version__ = "0.1.0"
